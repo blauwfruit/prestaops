c9a8471f3efe36a5a4aaa712b3829f16d8715f38
Matthijs <github@muynck.eu>
2025-03-07 12:49:06 +0100

    feat: Add constant for config file name

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 define('PRESTA_OPS_ROOT_DIR', getcwd() . '/');
+define('PRESTA_OPS_CONFIG_FILE_NAME', '.prestaops');
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/prestaops.php';
